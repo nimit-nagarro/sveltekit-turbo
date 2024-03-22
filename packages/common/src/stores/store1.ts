5e164c16be4969ac6300ca77a43108dfb682fc6c
Nimit Jain <nimit.jain@nagarro.com>
2024-03-22 16:42:59 +0530

    console added for checking barrel imports

--- packages/common/src/stores/store1.ts
+++ packages/common/src/stores/store1.ts
@@ -1,6 +1,6 @@
 // src/stores/Store1.js
 import { writable } from 'svelte/store';
-
+console.log("Hi I am called from store1.ts which is having count");
 export const count = writable(0);
 
 export function increment() {
--- packages/common/src/stores/store2.ts
+++ packages/common/src/stores/store2.ts
@@ -1,6 +1,6 @@
 // src/stores/Store2.js
 import { writable } from 'svelte/store';
-
+console.log("Hi I am called from store2.ts which is not invoked from anywhere");
 export const text = writable('Initial text');
 
 export function setText(newText) {
